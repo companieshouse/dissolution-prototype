2c0d4465b8e9f16a083d06cbd6a398407d3740bd
jfrancis@companieshouse.gov.uk <jfrancis@companieshouse.gov.uk>
2020-06-09 11:27:21 +0100

    Added variations to director choose

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,62 +2,87 @@
 /* global GOVUK */
 
 // Warn about using the kit in production
-if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+if ( window.console && window.console.info ) {
+	window.console.info( 'GOV.UK Prototype Kit - do not use for production' )
 }
 
-$(document).ready(function () {
-  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
-  // with role="button" when the space key is pressed.
-  GOVUK.shimLinksWithButtonRole.init()
+$( document )
+	.ready( function () {
+		window.GOVUKFrontend.initAll()
 
-  // Show and hide toggled content
-  // Where .multiple-choice uses the data-target attribute
-  // to toggle hidden content
-  var showHideContent = new GOVUK.ShowHideContent()
-  showHideContent.init()
+		// Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+		// with role="button" when the space key is pressed.
+		GOVUK.shimLinksWithButtonRole.init()
 
-  // Sign document button
-  $('.add-sig-box').click(function () {
-    $('.adopt').fadeIn(200, function () {
-      $('.adopt-window').fadeIn(200)
-    })
-    return false
-  })
+		// Show and hide toggled content
+		// Where .multiple-choice uses the data-target attribute
+		// to toggle hidden content
+		var showHideContent = new GOVUK.ShowHideContent()
+		showHideContent.init()
 
-  // Adopt signature button
-  $('.adopt-button').click(function () {
-    $('.add-sig-box').hide()
-    $('.adopt').hide()
-    $('.sign-tag').hide()
-    $('.signature').show()
-    $('.confirm-bar').animate({bottom: '0px'})
-    return false
-  })
+		// Sign document button
+		$( '.add-sig-box' )
+			.click( function () {
+				$( '.adopt' )
+					.fadeIn( 200, function () {
+						$( '.adopt-window' )
+							.fadeIn( 200 )
+					} )
+				return false
+			} )
 
-  // Cancel adopt signature
-  $('.cancel-button').click(function () {
-    $('.adopt').hide()
-    $('.adopt-window').hide()
-    return false
-  })
+		// Adopt signature button
+		$( '.adopt-button' )
+			.click( function () {
+				$( '.add-sig-box' )
+					.hide()
+				$( '.adopt' )
+					.hide()
+				$( '.sign-tag' )
+					.hide()
+				$( '.signature' )
+					.show()
+				$( '.confirm-bar' )
+					.animate( {
+						bottom: '0px'
+					} )
+				return false
+			} )
 
-  // Close adopt signature
-  $('.close-button').click(function () {
-    $('.adopt').hide()
-    $('.adopt-window').hide()
-    return false
-  })
+		// Cancel adopt signature
+		$( '.cancel-button' )
+			.click( function () {
+				$( '.adopt' )
+					.hide()
+				$( '.adopt-window' )
+					.hide()
+				return false
+			} )
 
-  // FAKE DOCUSIGN JS
-  $('#tab-form-element-473279d7-8970-4b24-90e2-bfe79a6ccbc1').click(function () {
-    $('#adopt-dialog').show()
-    return false
-  })
+		// Close adopt signature
+		$( '.close-button' )
+			.click( function () {
+				$( '.adopt' )
+					.hide()
+				$( '.adopt-window' )
+					.hide()
+				return false
+			} )
 
-  $('#adopt-my-sig').click(function () {
-    $('#adopt-dialog').hide()
-    $('#olive-dialog-wrapper').show()
-    return false
-  })
-})
+		// FAKE DOCUSIGN JS
+		$( '#tab-form-element-473279d7-8970-4b24-90e2-bfe79a6ccbc1' )
+			.click( function () {
+				$( '#adopt-dialog' )
+					.show()
+				return false
+			} )
+
+		$( '#adopt-my-sig' )
+			.click( function () {
+				$( '#adopt-dialog' )
+					.hide()
+				$( '#olive-dialog-wrapper' )
+					.show()
+				return false
+			} )
+	} )
