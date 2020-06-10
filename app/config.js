463ca500b9334d7c5dd78edb4febbfd0ac46c82a
jfrancis@companieshouse.gov.uk <jfrancis@companieshouse.gov.uk>
2020-06-10 07:45:10 +0100

    Updated service title

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
 	// Service name used in header. Eg: 'Renew your passport'
-	serviceName: 'Closing a limited company (DS01)',
+	serviceName: 'Closing a limited company',
 
 	// Default port that prototype runs on
 	port: '3000',
