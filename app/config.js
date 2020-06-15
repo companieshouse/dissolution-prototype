a8f37858daa0883a8eef66c61e00769fbc5fc304
Emma Lynham <elynham@companieshouse.gov.uk>
2020-06-15 10:41:51 +0100

    Changes to content throughout
    Including:
    - Change to service name
    - Revised wait to sign screens
    - Adjusted content for DS01/LLDS01 merge
    - Clarity around the PDF copy of the application
    - Added messaging to inform users there is no saving of progress

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
 	// Service name used in header. Eg: 'Renew your passport'
-	serviceName: 'Closing a limited company',
+	serviceName: 'Apply to strike off and dissolve a company',
 
 	// Default port that prototype runs on
 	port: '3000',
